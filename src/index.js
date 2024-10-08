bfe0e3c7637f9ae4f586c45e704ce821d96f2210
emesefedev <emesefedev@gmail.com>
2024-10-08 16:41:03 +0200

    feature: show 1 image

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import "./style.css";
 
-console.log("Hello");
+const imagesInfo = [
+  {
+    url: "https://www.freepik.es/imagen-ia-gratis/vista-camaleon-brillantes-colores-neon_266515770.htm#fromView=search&page=1&position=29&uuid=e384aa8f-d66e-4079-b3c4-5bf4224f2d0b",
+  },
+  {
+    url: "https://www.freepik.es/imagen-ia-gratis/vista-camaleon-brillantes-colores-neon_266515883.htm#fromView=search&page=1&position=37&uuid=093f6376-21ac-4f9c-ac94-f41b6b9e95b8",
+  },
+  {
+    url: "https://www.freepik.es/imagen-ia-gratis/vista-camaleon-brillantes-colores-neon_266515738.htm#fromView=search&page=4&position=38&uuid=093f6376-21ac-4f9c-ac94-f41b6b9e95b8",
+  },
+  {
+    url: "https://www.freepik.es/imagen-ia-gratis/colores-brillantes-neon-brillan-camaleon-salvaje_266517451.htm#fromView=search&page=1&position=2&uuid=98289ad6-54cc-4ba1-9787-e478a1633655",
+  },
+  {
+    url: "https://www.freepik.es/imagen-ia-gratis/vista-camaleon-brillantes-colores-neon_266515623.htm#fromView=search&page=1&position=39&uuid=46e1cd6e-b8f4-4ef3-964d-1ddd2463d3af",
+  },
+  {
+    url: "https://www.freepik.es/imagen-ia-gratis/hermoso-camaleon-naturaleza_269137670.htm#fromView=search&page=1&position=44&uuid=46e1cd6e-b8f4-4ef3-964d-1ddd2463d3af",
+  },
+];
